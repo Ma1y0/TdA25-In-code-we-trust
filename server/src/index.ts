854697f73aef9e838e3173628c7c18ea0cf79d41
Matyáš Bár <matyas.barr@gmail.com>
2024-11-01 09:55:40 +0100

    Pass the 1st stage

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,20 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/bun";
+import { api } from "./api";
 
 const app = new Hono();
 
 app.use(logger());
 
+// The API routes
+app.route("/api", api);
+
+// The static page
 app.use("/*", serveStatic({ root: "./client" }));
 app.get("*", serveStatic({ path: "./client/index.html" }));
 
 export default {
-  port: 80,
+  port: process.env.PORT ?? 80,
   fetch: app.fetch,
 };
